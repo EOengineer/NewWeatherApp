51b5bb7e0a67d1e0623c09c1e3c3ece78a7929e6
Eric Oligney <EOengineer@gmail.com>
2015-07-02 20:38:40 -0400

    mocked api response, ajaxified current weather form.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
 
   get 'current_weather', controller: :weathers, action: :index
-
+  post 'current_weather', controller: :weathers, action: :index
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
